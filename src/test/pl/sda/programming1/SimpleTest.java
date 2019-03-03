bdc8566d14fb0438bac2037f77a1371b5f6dd0c8
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-03 16:01:29 +0100

    Simple test

--- src/test/pl/sda/programming1/SimpleTest.java
+++ src/test/pl/sda/programming1/SimpleTest.java
@@ -1,11 +1,30 @@
 package pl.sda.programming1;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 public class SimpleTest {
 
     @Test
     public void shouldDoSomething() {
-        System.out.println("something");
+        CsvReader reader = new CsvReaderUtils();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("MARKA", "ALFA ROMEO");
+        map.put("MODEL", "GULIETTA");
+        map.put("ROK", 2012);
+        map.put("KM", 300);
+        map.put("CENA", BigDecimal.TEN);
+
+        List<Car> result = reader.objects(Collections.singletonList(map));
+
+        assert result.size() == 1;
+        Car car = result.get(0);
+        assert car.getPowerInKM() == 300;
+        Assertions.assertThat(result).hasSize(1);
+        Assertions.assertThat(car.getModelName()).isEqualToIgnoringCase("gulietta");
     }
 }
