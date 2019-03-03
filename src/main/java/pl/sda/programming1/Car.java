c5569e9590cf59664f470a9679ec12c889ce64b7
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-03 10:14:23 +0100

    Commons example

--- src/main/java/pl/sda/programming1/Car.java
+++ src/main/java/pl/sda/programming1/Car.java
@@ -1,4 +1,16 @@
 package pl.sda.programming1;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Car {
+
+    private String name;
+
+    public Car() {
+        this.name = RandomStringUtils.randomAlphabetic(10);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
