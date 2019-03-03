0cdc2eefecc74dc5312736a22d2bd0034d0ba4f5
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2019-03-03 11:06:13 +0100

    Added reading files

--- src/main/java/pl/sda/programming1/CsvReaderUtils.java
+++ src/main/java/pl/sda/programming1/CsvReaderUtils.java
@@ -1,4 +1,42 @@
 package pl.sda.programming1;
 
-public class CsvReaderUtils {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CsvReaderUtils implements CsvReader {
+
+
+
+    @Override
+    public List<String[]> readLines(File file) {
+        String[] carParameters;
+        List<String[]> listOfString = new ArrayList<>();
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            int i = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (i != 0) {
+                    carParameters = line.split(";");
+                    listOfString.add(carParameters);
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listOfString;
+    }
+
+    @Override
+    public List<Map<String, Object>> convert(List<String[]> lines) {
+        return null;
+    }
+
+    @Override
+    public List<Car> objects(List<Map<String, Object>> values) {
+        return null;
+    }
 }
--- src/main/java/pl/sda/programming1/Main.java
+++ src/main/java/pl/sda/programming1/Main.java
@@ -1,10 +1,17 @@
 package pl.sda.programming1;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car();
-        System.out.println(car.getName());
+        CsvReaderUtils csvReaderUtils = new CsvReaderUtils();
+        List<String[]> listOfCars = csvReaderUtils.readLines(new File("/home/shaders/SDA_Learning/Java/java-programing-I/sda-java-project-csv-reader/cars.csv"));
+        listOfCars.forEach(record -> {
+            System.out.println(Arrays.toString(record));
+        });
     }
 
 }
