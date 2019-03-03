9ed488bdb078a1b64b25249ec9e249b55160bb20
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-03 15:15:12 +0100

    Example test

---
+++ src/test/pl/sda/programming1/SimpleTest.java
@@ -0,0 +1,11 @@
+package pl.sda.programming1;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void shouldDoSomething() {
+        System.out.println("something");
+    }
+}
