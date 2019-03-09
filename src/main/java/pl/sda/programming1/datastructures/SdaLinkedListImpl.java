4b9143b4fb913727d208884c86f51578c7f073f8
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-09 13:05:26 +0100

    SDA Linked List WIP2

--- src/main/java/pl/sda/programming1/datastructures/SdaLinkedListImpl.java
+++ src/main/java/pl/sda/programming1/datastructures/SdaLinkedListImpl.java
@@ -1,12 +1,14 @@
 package pl.sda.programming1.datastructures;
 
+import java.util.NoSuchElementException;
+
 public class SdaLinkedListImpl<T> implements SdaLinkedList<T> {
 
     private Node<T> head;
 
     @Override
     public boolean isEmpty() {
-        return head != null;
+        return head == null;
     }
 
     @Override
@@ -16,18 +18,26 @@ public class SdaLinkedListImpl<T> implements SdaLinkedList<T> {
 
     @Override
     public T getFirst() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("The list is empty! Nothing to get!");
+        }
+        return head.element;
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("The list is empty! Nothing to get!");
+        }
+        return nodeLast().element;
     }
 
     @Override
     public T get(int index) {
         return null;
     }
+
+
 
     @Override
     public void addFirst(T element) {
@@ -36,7 +46,22 @@ public class SdaLinkedListImpl<T> implements SdaLinkedList<T> {
 
     @Override
     public void addLast(T element) {
+        if (isEmpty()) {
+            addFirst(element);
+        } else {
+            nodeLast().next = new Node<>(element, null);
+        }
+    }
 
+    private Node<T> nodeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("The list is empty! You cannot do that!");
+        }
+        Node<T> last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        return last;
     }
 
     @Override
--- src/test/pl/sda/programming1/ListTest.java
+++ src/test/pl/sda/programming1/ListTest.java
@@ -2,10 +2,14 @@ package pl.sda.programming1;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.sda.programming1.datastructures.SdaLinkedList;
 import pl.sda.programming1.datastructures.SdaLinkedListImpl;
 
 public class ListTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ListTest.class);
 
     @Test
     public void testSdaLinkedList() {
@@ -14,5 +18,10 @@ public class ListTest {
         Assertions.assertThat(sdaLinkedList.isEmpty()).isTrue();
         sdaLinkedList.addFirst(10);
         Assertions.assertThat(sdaLinkedList.isEmpty()).isFalse();
+        sdaLinkedList.addLast(99);
+
+        Assertions.assertThat(sdaLinkedList.getFirst()).isEqualTo(10);
+        log.info("First element={}", sdaLinkedList.getFirst());
+        log.info("Last element={}", sdaLinkedList.getLast());
     }
 }
