cabef47443081926683523f2fba7ce398b9dc628
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-09 11:43:35 +0100

    Recursion changes

--- src/main/java/pl/sda/programming1/Main.java
+++ src/main/java/pl/sda/programming1/Main.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
---
+++ src/main/java/pl/sda/programming1/recursion/GDC.java
@@ -0,0 +1,47 @@
+package pl.sda.programming1.recursion;
+
+public class GDC {
+
+    public static void main(String[] args) {
+//        System.out.println(recursiveGdc(255, 102));
+        System.out.println(recursiveFibonacci(5));
+    }
+
+    public static int recursiveFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            int fibonacciNMinus1 = recursiveFibonacci(n - 1);
+            int fibonacciNMinus2 = recursiveFibonacci(n - 2);
+            int fibResult = fibonacciNMinus1 + fibonacciNMinus2;
+            return fibResult;
+        }
+    }
+
+    public static int recursiveGdc(int a, int b) {
+        if (a == b) {
+            return a;
+        } else {
+            // skrócony if (tenary operator)
+            int newA = a > b ? a - b : a;
+//            odpowiednik powyższej linijki w dłuższej formie (normalny if)
+//            int newAA;
+//            if (a > b) {
+//                newAA = a - b;
+//            } else {
+//                newAA = a;
+//            }
+            int newB = b > a ? b - a : b;
+//            taka sama sytuacja jak w przypadku wyliczania 'newA'
+//            int newBB;
+//            if (b > a) {
+//                newBB = b - a;
+//            } else {
+//                newBB = b;
+//            }
+            int recursiveExecutionResult = recursiveGdc(newA, newB);
+            return recursiveExecutionResult;
+        }
+    }
+
+}
